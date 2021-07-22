5c75438e1f7df5dda9e19bc1bd98ce5115c8d8c8
hlld <1161985414@qq.com>
2021-07-22 20:15:53 +0800

    Dataset prepare

---
+++ classification/dataset.py
@@ -0,0 +1,87 @@
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+def create_dataloader(args,
+                      use_transet=True,
+                      return_info=True):
+    if args.data_type == 'imagenet':
+        imagedir = 'train' if use_transet else 'val'
+        datadir = os.path.join(args.data_path, imagedir)
+        num_classes = 1000
+        image_size = 224
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        base_ops = [transforms.ToTensor(),
+                    transforms.Normalize(mean, std)]
+        if use_transet:
+            aug_ops = [transforms.RandomResizedCrop(image_size),
+                       transforms.RandomHorizontalFlip()]
+        else:
+            aug_ops = [transforms.Resize(256),
+                       transforms.CenterCrop(image_size)]
+        base_ops = aug_ops + base_ops
+        transform = transforms.Compose(base_ops)
+        dataset = datasets.ImageFolder(root=datadir,
+                                       transform=transform)
+    elif args.data_type in ['mnist', 'cifar10', 'cifar100', 'svhn']:
+        mean = [0.5, 0.5, 0.5]
+        std = [0.5, 0.5, 0.5]
+        image_size = 32
+        if args.data_type == 'mnist':
+            builder = torchvision.datasets.MNIST
+            num_classes = 10
+        elif args.data_type == 'cifar10':
+            builder = torchvision.datasets.CIFAR10
+            num_classes = 10
+        elif args.data_type == 'cifar100':
+            builder = torchvision.datasets.CIFAR100
+            num_classes = 100
+        else:
+            builder = torchvision.datasets.SVHN
+            num_classes = 10
+        if args.data_type == 'mnist':
+            base_ops = [transforms.ToTensor(),
+                        transforms.Normalize(mean[0], std[0])]
+        else:
+            base_ops = [transforms.ToTensor(),
+                        transforms.Normalize(mean, std)]
+        if use_transet:
+            padding = 6 if args.data_type == 'mnist' else 4
+            aug_ops = [transforms.RandomCrop(image_size, padding=padding),
+                       transforms.RandomHorizontalFlip()]
+            base_ops = aug_ops + base_ops
+        elif args.data_type == 'mnist':
+            aug_ops = [transforms.Pad(padding=2)]
+            base_ops = aug_ops + base_ops
+        transform = transforms.Compose(base_ops)
+        if args.data_type == 'svhn':
+            split_set = 'train' if use_transet else 'test'
+            dataset = builder(root=args.data_path,
+                              split=split_set,
+                              download=True,
+                              transform=transform)
+        else:
+            dataset = builder(root=args.data_path,
+                              train=use_transet,
+                              download=True,
+                              transform=transform)
+    else:
+        raise ValueError('Unknown data type %s' % args.data_type)
+    batch_size = args.test_batch_size
+    if use_transet:
+        batch_size = args.train_batch_size
+    sampler = None
+    if use_transet and args.use_ddp:
+        sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+    # For Windows use num_workers=0
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=use_transet and not args.use_ddp,
+        num_workers=args.num_workers,
+        pin_memory=True,
+        sampler=sampler)
+    if return_info:
+        return dataloader, [image_size, num_classes]
+    return dataloader
---
+++ classification/tools.py
@@ -0,0 +1,132 @@
+import os
+import numpy as np
+import torch
+import random
+from contextlib import contextmanager
+
+
+def load_yaml(yaml_path):
+    # Check and load yaml file
+    if not os.path.isfile(yaml_path):
+        raise ValueError('Load %s failed' % yaml_path)
+    with open(yaml_path) as fd:
+        yaml_dict = yaml.load(fd, Loader=yaml.SafeLoader)
+    return yaml_dict
+
+
+def save_yaml(file_path, yaml_dict):
+    with open(file_path, 'w') as fd:
+        yaml.dump(yaml_dict,
+                  fd,
+                  sort_keys=False)
+
+
+def select_device(device):
+    if isinstance(device, int):
+        device = '%d' % device
+    if device.lower() == 'cpu':
+        # Force torch.cuda.is_available() to False
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    else:
+        os.environ['CUDA_VISIBLE_DEVICES'] = device
+    device = 'cpu'
+    if torch.cuda.is_available():
+        # Select device 0 for only one visible device
+        device = 'cuda:0'
+    return torch.device(device)
+
+
+def torch_benchmark(benchmark=False, local_rank=-1):
+    # Setup fixed or random training seed
+    fixed_seed = local_rank + 1
+    torch.manual_seed(fixed_seed)
+    random.seed(fixed_seed)
+    np.random.seed(fixed_seed)
+    if benchmark:
+        # Train slower but more reproducible
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+    else:
+        # Train faster but less reproducible
+        torch.backends.cudnn.benchmark = True
+        torch.backends.cudnn.deterministic = False
+
+
+@contextmanager
+def torch_zero_rank_first(local_rank):
+    # https://github.com/ultralytics/yolov5
+    # Make sure only the first local process in DDP process first
+    # The following others can use the cache
+    if local_rank not in [-1, 0]:
+        torch.distributed.barrier()
+    yield
+    if local_rank == 0:
+        torch.distributed.barrier()
+
+
+def decay_lambda(final_ratio, 
+                 total_steps, 
+                 linear_decay=False):
+    # Linear or cosine decay
+    if linear_decay:
+        lambda_fn = lambda x: final_ratio + (1.0 - final_ratio) * (
+            1.0 - x / total_steps)
+    else:
+        lambda_fn = lambda x: 1.0 + (final_ratio - 1.0) * (
+                (1.0 - math.cos(x * math.pi / total_steps)) / 2)
+    return lambda_fn
+
+
+def find_directory(root_path, base_name):
+    found_path = False
+    index = 0
+    while not found_path:
+        index += 1
+        save_path = '%s/%s%d' % (root_path,
+                                 base_name,
+                                 index)
+        if not os.path.exists(save_path):
+            found_path = True
+    return save_path
+
+
+class ModelEMA(object):
+    # https://github.com/rwightman/pytorch-image-models
+    def __init__(self, model, decay=0.9999):
+        self.model = deepcopy(model).eval()
+        self.updates = 0
+        # Decay exponential ramp (to help early epochs)
+        self.decay_lambda = lambda x: decay * (1 - math.exp(-x / 2000))
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+    def update(self, model):
+        # Update EMA parameters
+        with torch.no_grad():
+            self.updates += 1
+            decay = self.decay_lambda(self.updates)
+            state_dict = model.state_dict()
+            for k, v in self.model.state_dict().items():
+                if v.dtype.is_floating_point:
+                    v *= decay
+                    v += (1.0 - decay) * state_dict[k].detach()
+
+
+def strip_optimizer(ckpt_path):
+    # Strip optimizer from ckpt_path to finalize training
+    device = torch.device('cpu')
+    ckpt = torch.load(ckpt_path, map_location=device)
+    if ckpt.get('model_ema'):
+        ckpt['model'] = ckpt['model_ema']
+    ckpt['model_ema'] = None
+    ckpt['updates'] = None
+    ckpt['optimizer'] = None
+    ckpt['epoch'] = -1
+    if torch.cuda.is_available():
+        ckpt['model'].half()
+    for p in ckpt['model'].parameters():
+        p.requires_grad = False
+    torch.save(ckpt, ckpt_path)
+    ckpt_size = os.path.getsize(ckpt_path) / 1e6
+    print('Optimizer stripped from %s, '
+          'total file size %g MB' % (ckpt_path, ckpt_size))
